2a1dc655acac65976d762a25108e1580c7699c94
pmukesh <pmukeshai@gmail.com>
2018-05-29 01:16:38 +0530

    changed to function

--- student/task6_1.js
+++ student/task6_1.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
-
-fs.readFile('students.txt', function(err, data) {  
-    if (err) throw err;
-    var student = JSON.parse(data);
-    console.log(student);
+function readtextFile() {
+    const fs = require('fs');
+    fs.readFile('students.txt', function(err, data) {  
+         if (err) {throw err;}
+         var student = JSON.parse(data);
+         console.log(student);
 });
 
+}
+readtextFile();
 console.log('This is after the read call');  
